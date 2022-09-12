e8a39a162c0106e51ee1e3c809eeb2adbc715a0c
ayrton <ayrton.parkinson1@gmail.com>
2022-09-12 06:55:36 -0400

    Add hit method to ship

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -1,9 +1,14 @@
 const shipFactory = (length) => {
   const condition = new Array(length).fill(0);
 
+  const hit = (position) => {
+    condition[position] = 1;
+  };
+
   return {
     length,
     condition,
+    hit,
   };
 };
 
