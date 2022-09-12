230c523888a0b0f05121154a3a49ce0680969ae1
ayrton <ayrton.parkinson1@gmail.com>
2022-09-12 06:55:27 -0400

    Add shipFactory test for ships hit method

--- jest/shipFactory.test.js
+++ jest/shipFactory.test.js
@@ -15,3 +15,11 @@ test('calling ship factory function with integer returns new ship with condition
   expect(shipLengthThree.condition).toEqual([0, 0, 0]);
   expect(shipLengthTen.condition).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 });
+
+test('hit takes a number and then marks the condition array as hit in that position', () => {
+  const ship = shipFactory(5);
+  ship.hit(0);
+  ship.hit(4);
+
+  expect(ship.condition).toEqual([1, 0, 0, 0, 1]);
+});
