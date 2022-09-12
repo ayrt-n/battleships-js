3d2c6ceacce715213bb8669ca4be97133c59eb68
ayrton <ayrton.parkinson1@gmail.com>
2022-09-12 07:13:46 -0400

    Add test for gameboard props at instantiation

---
+++ jest/gameboardFactory.test.js
@@ -0,0 +1,18 @@
+import gameboardFactory from "../src/gameboardFactory";
+
+test('instantiating new gameboard creates 10x10 matrix of arrays', () => {
+  const gameboard = gameboardFactory();
+
+  expect(gameboard.board).toEqual([
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+    [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
+  ]);
+});
