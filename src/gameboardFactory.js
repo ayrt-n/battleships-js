fd5ca1b3f08a9c440201274fba9a6a2fb85d3c6e
ayrton <ayrton.parkinson1@gmail.com>
2022-09-12 07:48:04 -0400

    Create placeShip method

--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -1,8 +1,25 @@
+import shipFactory from './shipFactory';
+
 const gameboardFactory = () => {
   const board = new Array(10).fill(new Array(10));
 
+  const placeShip = (...coordinates) => {
+    const ship = shipFactory(coordinates.length);
+
+    coordinates.forEach((coordinate, index) => {
+      const row = coordinate[0];
+      const col = coordinate[1];
+
+      board[row][col] = {
+        ship,
+        index,
+      };
+    });
+  };
+
   return {
     board,
+    placeShip,
   };
 };
 
