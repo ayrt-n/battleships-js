d5a58f6827b9c1c180cf10d7869ce39a442b094d
ayrton <ayrton.parkinson1@gmail.com>
2022-09-12 07:47:51 -0400

    Add test for placeShip

--- jest/gameboardFactory.test.js
+++ jest/gameboardFactory.test.js
@@ -16,3 +16,12 @@ test('instantiating new gameboard creates 10x10 matrix of arrays', () => {
     [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
   ]);
 });
+
+test('placeShip creates ship and places at specific coordinates', () => {
+  const gameboard = gameboardFactory();
+  gameboard.placeShip([0, 0], [0, 1], [0, 2]);
+
+  expect(gameboard.board[0][0].ship).toBeTruthy();
+  expect(gameboard.board[0][1].ship).toBeTruthy();
+  expect(gameboard.board[0][2].ship).toBeTruthy();
+});
