24747d206b1ef4dbc03fade694ae9053a6e00ec8
ayrton <ayrton.parkinson1@gmail.com>
2022-09-13 07:51:58 -0400

    Add test for player attack

---
+++ jest/player.test.js
@@ -0,0 +1,12 @@
+import playerFactory from "../src/player";
+
+test('attack sends enemy gameboard receiveAttack with specified coordinates', () => {
+  const receiveAttack = jest.fn(() => {});
+  const fakeEnemyGameboard = { receiveAttack };
+
+  const player = playerFactory(fakeEnemyGameboard);
+  player.attack([0, 0]);
+
+  expect(receiveAttack.mock.calls.length).toBe(1);
+  expect(receiveAttack.mock.calls[0][0]).toEqual([0, 0]);
+});
