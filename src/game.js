acbb05ff1a75f1484ac06d8d7bb6f783cd7a682c
ayrton <ayrton.parkinson1@gmail.com>
2022-09-16 06:48:43 -0400

    Add gameflow event listener to allow player and computer to make moves

--- src/game.js
+++ src/game.js
@@ -18,10 +18,46 @@ const game = () => {
     });
   };
 
+  // Check for winner and return Player, Computer, or undefined if no winner
+  const checkWinner = () => {
+    if (computerGameboard.isGameOver()) {
+      return 'player';
+    }
+    if (playerGameboard.isGameOver()) {
+      return 'computer';
+    }
+
+    return undefined;
+  };
+
+  // Starts a new game and reinitialize dom
   const start = () => {
     placeShips();
     dom.initializeDom(playerGameboard.board, computerGameboard.board);
   };
+
+  // Event listener for player moves
+  dom.computerBoardContainer.addEventListener('click', (e) => {
+    const square = e.target.closest('.square');
+    if (!square || square.classList.contains('hit')) { return; }
+
+    // Make the players move
+    const playerRow = Number(square.getAttribute('data-row'));
+    const playerCol = Number(square.getAttribute('data-col'));
+    dom.attackBoard(playerRow, playerCol, dom.computerBoardContainer);
+    player.attack([playerRow, playerCol]);
+
+    // Check for winner
+    console.log(checkWinner());
+
+    // Make the computers move
+    const [computerRow, computerCol] = computer.randomAttack();
+    dom.attackBoard(computerRow, computerCol, dom.playerBoardContainer);
+    playerGameboard.receiveAttack([computerRow, computerCol]);
+
+    // Check for winner
+    console.log(checkWinner());
+  });
 
   return {
     start,
