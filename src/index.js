93ea032f162ff5602857911fc0c0d921e77bf332
ayrton <ayrton.parkinson1@gmail.com>
2022-09-17 08:39:16 -0400

    Fix bug with reset button by ending current game first

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ battleship.startGame();
 
 // Instantiate new battleship game and load
 resetBtn.addEventListener('click', () => {
+  // End current game
+  battleship.endGame();
+
+  // Instantiate new game
   battleship = game();
   battleship.startGame();
 });
